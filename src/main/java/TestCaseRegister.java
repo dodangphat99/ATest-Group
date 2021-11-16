36fea5ef683b6006a7a1b8cd1b61e98a77fd0b79
Phat Do <dodangphat99@gmail.com>
2021-11-16 18:27:24 +0700

    commit 3

--- src/main/java/TestCaseRegister.java
+++ src/main/java/TestCaseRegister.java
@@ -5,7 +5,14 @@ public class TestCaseRegister {
     public static void main(String[] args){
         RegisterPage rp = new RegisterPage();
         User user = new User();
-        user.setHoten("");
+        user.setFullName("do dang phat");
+        user.setPhoneNumber("0913427279");
+        user.setEmail("toi@gmail.com");
+        user.setNewPassword("1234567");
+        user.setConfirmPassword("1234567");
+        user.setUserName("toi@gmail.com");
+        user.setPassword("1234567");
+        user.setSearch("bot giat");
         rp.runRegister(user);
     }
 }
