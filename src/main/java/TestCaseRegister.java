bdf972cb76f9627712626ce1ddf01a3780e7e758
Phat Do <dodangphat99@gmail.com>
2021-11-22 08:03:06 +0700

    update 4

--- src/main/java/TestCaseRegister.java
+++ src/main/java/TestCaseRegister.java
@@ -1,25 +1,24 @@
-import model.User;
 import org.testng.annotations.Test;
-import page.RegisterPage;
+import pages.RegisterPage;
 
 public class TestCaseRegister {
     @Test
     public void main(){
         RegisterPage rp = new RegisterPage();
-        User user = getTestUser();
-        rp.register(user);
+//        User user = getTestUser();
+        rp.register();
     }
 
-    private static User getTestUser(){
-        User user = new User();
-        user.setFullName("do dang phat");
-        user.setPhoneNumber("0913427296");
-        user.setEmail("toiiiiiiiiiiiiiiiiii@gmail.com");
-        user.setNewPassword("1234567");
-        user.setConfirmPassword("1234567");
-        user.setUserName("toiiiiiiiiiiiiiiiiii@gmail.com");
-        user.setPassword("1234567");
-        user.setSearch("bot giat");
-        return user;
-    }
+//    private static User getTestUser(){
+//        User user = new User();
+//        user.setFullName("do dang phat");
+//        user.setPhoneNumber("0913427218");
+//        user.setEmail("toiiiiiiiiiiiiiiiiiiiiiiiiiiiiii@gmail.com");
+//        user.setNewPassword("1234567");
+//        user.setConfirmPassword("1234567");
+//        user.setUserName("toiiiiiiiiiiiiiiiiiiiiiiiiiiiiii@gmail.com");
+//        user.setPassword("1234567");
+//        user.setSearch("bot giat");
+//        return user;
+//    }
 }
