b2c585f1fc9f09beafc82c890e02cd3aa38ec236
Phat Do <dodangphat99@gmail.com>
2021-12-02 18:21:19 +0700

    update

--- src/main/java/reports/Reporter.java
+++
@@ -1,9 +0,0 @@
-package reports;
-
-public class Reporter {
-    private static final String VERIFY_MESSAGE_FORMAT = "<em>&emsp;&emsp;+";
-
-    private static String formatVerifyMessage(final String message){
-        return String.format(VERIFY_MESSAGE_FORMAT,message);
-    }
-}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,22 +3,26 @@ package tests;
 import commons.GlobalVariable;
 import helpers.DriverHelper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-//    public static Actions actions = new Actions(GlobalVariable.getDriver());
-//    private static WebElement bodyXpath = By.xpath("//body");
 
     @BeforeClass
     public static void before(){
         GlobalVariable.setDriver();
+        Actions actions = new Actions(GlobalVariable.getDriver());
         GlobalVariable.getDriver().navigate().to("https://fumart.vn/");
+        WebElement elementLocator = GlobalVariable.getDriver().findElement(By.xpath("//body"));
         GlobalVariable.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        actions.contextClick(bodyXpath).perform();
+        actions.contextClick(elementLocator).build().perform();
     }
 
 }
