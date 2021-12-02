1fd1d09c4bafe411d28202e65c10781166177081
Phat Do <dodangphat99@gmail.com>
2021-12-02 16:15:35 +0700

    update code new

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,17 +2,23 @@ package tests;
 
 import commons.GlobalVariable;
 import helpers.DriverHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
+//    public static Actions actions = new Actions(GlobalVariable.getDriver());
+//    private static WebElement bodyXpath = By.xpath("//body");
 
     @BeforeClass
     public static void before(){
         GlobalVariable.setDriver();
         GlobalVariable.getDriver().navigate().to("https://fumart.vn/");
         GlobalVariable.getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        actions.contextClick(bodyXpath).perform();
     }
 
 }
